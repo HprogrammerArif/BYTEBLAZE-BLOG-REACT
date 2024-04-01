7359bd23685867ebbf159483a18a91542067a048
HprogrammerArif <programmerhabluarif@gmail.com>
2024-04-01 14:25:41 +0600

    Creating react project and setup navbar and some instlation

---
+++ src/pages/Blogs.jsx
@@ -0,0 +1,10 @@
+
+const Blogs = () => {
+  return (
+    <div>
+      <h2>This Is Blogs Page</h2>
+    </div>
+  );
+};
+
+export default Blogs;
---
+++ src/pages/Bookmarks.jsx
@@ -0,0 +1,10 @@
+
+const Bookmarks = () => {
+  return (
+    <div>
+      <h2>This Is Bookmarks Page</h2>
+    </div>
+  );
+};
+
+export default Bookmarks;
