0d0ead8f3a0d826b8b9d0e07787a11d9b5e55962
HprogrammerArif <programmerhabluarif@gmail.com>
2024-04-02 16:50:29 +0600

    added local storage

--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -1,8 +1,30 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { deleteBlog, getBlogs } from "../utils/LocalStorage";
+import BlogCard from "../components/BlogCard/BlogCard";
+
 const Bookmarks = () => {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    const storedBlogs = getBlogs();
+    setBlogs(storedBlogs);
+  }, []);
+
+  const handleDelete = (id) => {
+    deleteBlog(id);
+    const storedBlogs = getBlogs();
+    setBlogs(storedBlogs);
+  };
+
   return (
-    <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-      {blogs.slice(1, 19).map((blog) => (
-        <BlogCard blog={blog} key={blog.id}></BlogCard>
+    <div className="grid px-4 py-8 sm:px-8 lg:px-12 justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      {blogs.map((blog) => (
+        <BlogCard
+          handleDelete={handleDelete}
+          deleteAble={true}
+          blog={blog}
+          key={blog.id}
+        ></BlogCard>
       ))}
     </div>
   );
