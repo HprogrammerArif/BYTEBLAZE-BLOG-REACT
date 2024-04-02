240b3bc8c4007074bf2a640b11ea7643b9d0a858
HprogrammerArif <programmerhabluarif@gmail.com>
2024-04-02 18:50:23 +0600

    add product remove section

--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { deleteBlog, getBlogs } from "../utils/LocalStorage";
 import BlogCard from "../components/BlogCard/BlogCard";
+import EmptyState from "../components/EmptyState/EmptyState";
 
 const Bookmarks = () => {
   const [blogs, setBlogs] = useState([]);
@@ -16,8 +17,12 @@ const Bookmarks = () => {
     setBlogs(storedBlogs);
   };
 
+  if (blogs<1) {
+    return <EmptyState address={'/blogs'} lable={'Go TO Blogs'} message={'No Bookmarks Abailable'}></EmptyState>
+  }
+
   return (
-    <div className="grid px-4 py-8 sm:px-8 lg:px-12 justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+    <div className="grid px-4 py-8 sm:px-8 lg:px-12 justify-center grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
       {blogs.map((blog) => (
         <BlogCard
           handleDelete={handleDelete}
